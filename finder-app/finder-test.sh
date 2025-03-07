635e15c4098a34c0133f26066dc825d41b52a494
Anton <droant3@gmail.com>
2025-03-07 21:44:13 +0900

    fixed path

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat /conf/username.txt)     #file path update to support target rootfs
+username=$(cat /etc/finder-app/conf/)     #file path update to support target rootfs
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/`
 
 if [ $assignment != 'assignment1' ]
 then
