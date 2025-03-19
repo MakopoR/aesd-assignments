a471f2ce5b4bcd85b4a4365404a1c6df57725fa9
Anton <droant3@gmail.com>
2025-03-19 19:42:04 +0900

    completed socket threading

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,6 +1,15 @@
 #include "aesdsocket.h"
 
 bool is_daemon = false;
+int buffersize = 128;
+
+struct slist_data_s
+{
+	struct thread_data *thread_struct;
+	pthread_t *thread;
+	SLIST_ENTRY(slist_data_s) entries;
+};
+
 
 /*SIGNAL FUNC*****************/
 bool caught_sigint = false;
@@ -14,10 +23,73 @@ static void signal_handler (int signal_number)
 	}
 }
 
+void *threadfunc(void* thread_param)
+{
+	int error = 0;
+	struct thread_data *thread_param_ = (struct thread_data*)thread_param;
+    //usleep(thread_param_->wait_to_obtain_ms * 1000);
+    syslog(LOG_INFO,"Start thread");
+
+
+	int i = 1;
+	char *buffer = (char*) calloc(buffersize * i, sizeof(char));
+	int received_packet_size = 0;
+	while(1)
+	{
+		received_packet_size = recv(*thread_param_->connfd,buffer+(buffersize * (i - 1)),buffersize, 0);
+		//send(connfd,buffer,received_packet_size,0);
+		//break;
+		
+		if(received_packet_size == buffersize) {
+			char *buffer_t = (char*) calloc(buffersize * i, sizeof(char));
+			memcpy(buffer_t, buffer, buffersize * i);
+			i++;
+			free(buffer);
+			buffer = (char*) calloc(buffersize * i, sizeof(char));
+			memcpy(buffer, buffer_t, buffersize * (i-1));
+		}
+		else {
+			syslog(LOG_INFO,"%s",buffer);
+			error = pthread_mutex_lock(thread_param_->mutex);
+				write(*thread_param_->packet_memory_fd,buffer,buffersize*(i-1)+received_packet_size); 
+		    	lseek(*thread_param_->packet_memory_fd, 0, SEEK_SET); //get back to the beginning
+			error =  pthread_mutex_unlock(thread_param_->mutex);
+				char buf[100];
+    			int read_bytes;
+    			int file_read=0;
+			while(file_read == 0)
+			{
+				error = pthread_mutex_lock(thread_param_->mutex);
+				read_bytes = read(*thread_param_->packet_memory_fd,buf,100);
+				error =  pthread_mutex_unlock(thread_param_->mutex);
+				if(read_bytes < 100) {
+					send(*thread_param_->connfd,buf,read_bytes,0);
+					break;
+				}
+				else {
+					send(*thread_param_->connfd,buf,100,0);
+				}
+			}
+			break;
+		}
+	}
+
+	close(*thread_param_->connfd);
+	syslog(LOG_INFO,"Closed connection from %d",thread_param_->clnt_addr.sin_addr.s_addr);
+	thread_param_->complete = true;
+	//return thread_param;
+	pthread_exit(thread_param);
+}
+
 int main(int argc, char *argv[])
 {
 /*OPEN LOG*****************/
 openlog(NULL,0,LOG_USER);
+/*INIT LIST*****************/
+SLIST_HEAD(slisthead, slist_data_s) head;
+//head_t head;
+SLIST_INIT(&head); // initialize the head
+
 /*REGISTER SIGNALS*****************/
 struct sigaction new_action;
 memset(&new_action,0,sizeof(struct sigaction));
@@ -37,6 +109,10 @@ if(argc == 2)
 		is_daemon = true;
 	}
 }
+
+/*CREATE MUTEX*****************/
+pthread_mutex_t file_mutex;
+pthread_mutex_init(&file_mutex,NULL);
 /*CREATE SOCKET*****************/
 int sockfd = socket(PF_INET,SOCK_STREAM,0);
 
@@ -98,7 +174,7 @@ if(kidpid == 0)
 {
 printf("child");
 printf("addr: %s\n",servinfo->ai_addr->sa_data);
-if(listen(sockfd, 10) != 0) {
+if(listen(sockfd, 100) != 0) {
         syslog(LOG_ERR,"ERROR listen");
         //return -1;
 	exit(1);
@@ -114,54 +190,45 @@ if(packet_memory_fd == -1) {
 ////////////////////////////////
 int connfd = 0;
 struct sockaddr_in clnt_addr;
-int buffersize = 128;
+
 socklen_t clnt_addr_size = sizeof(clnt_addr);
 while(1)
 {
 	connfd = accept(sockfd, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+	if(connfd == -1)
+		continue;
 	syslog(LOG_INFO,"Accepted connection from %d",clnt_addr.sin_addr.s_addr);
 	
-	int i = 1;
-	char *buffer = (char*) calloc(buffersize * i, sizeof(char));
-	int received_packet_size = 0;
-	while(1)
-	{
-		received_packet_size = recv(connfd,buffer+(buffersize * (i - 1)),buffersize, 0);
-		//send(connfd,buffer,received_packet_size,0);
-		//break;
-		
-		if(received_packet_size == buffersize) {
-			char *buffer_t = (char*) calloc(buffersize * i, sizeof(char));
-			memcpy(buffer_t, buffer, buffersize * i);
-			i++;
-			free(buffer);
-			buffer = (char*) calloc(buffersize * i, sizeof(char));
-			memcpy(buffer, buffer_t, buffersize * (i-1));
-		}
-		else {
-			write(packet_memory_fd,buffer,buffersize*(i-1)+received_packet_size); 
-		    	lseek(packet_memory_fd, 0, SEEK_SET); //get back to the beginning
-			char buf[100];
-    			int read_bytes;
-    			int file_read=0;
-			while(file_read == 0)
-			{
-				read_bytes = read(packet_memory_fd,buf,100);
-				if(read_bytes < 100) {
-					send(connfd,buf,read_bytes,0);
-					break;
-				}
-				else {
-					send(connfd,buf,100,0);
-				}
-			}
-			break;
+	struct thread_data* data = malloc(sizeof(struct thread_data));
+	if(data) {
+		data->connfd = &connfd;
+		data->packet_memory_fd = &packet_memory_fd;
+		data->clnt_addr = clnt_addr;
+		data->mutex = &file_mutex;
+		data->complete = false;
+	}
+	else
+		return false;
+
+	/*CREATE MUTEX*****************/
+	pthread_t *thread = malloc(sizeof(pthread_t));
+	pthread_mutex_t file_mutex;
+
+	struct slist_data_s * e = NULL;
+	e = malloc(sizeof(struct slist_data_s));
+	e->thread = thread;
+	e->thread_struct = data;
+	SLIST_INSERT_HEAD(&head,e,entries);
+	void *ret;
+	int error = pthread_create(thread,NULL,threadfunc,data);
+	struct slist_data_s * datap = NULL;
+	SLIST_FOREACH(datap, &head, entries) {
+		if(datap->thread_struct->complete == true)
+		{
+			pthread_join(*datap->thread,ret);
 		}
 	}
 
-	close(connfd);
-	syslog(LOG_INFO,"Closed connection from %d",clnt_addr.sin_addr.s_addr);
-	
 	if(caught_sigint) {
 		syslog(LOG_INFO,"Caught signal, exiting");
 		break;
--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include "sys/queue.h" // queue taken from FreeBSD 10
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,4 +13,24 @@
 #include <signal.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <pthread.h>
 
+struct thread_data{
+    /*
+     * TODO: add other values your thread will need to manage
+     * into this structure, use this structure to communicate
+     * between the start_thread_obtaining_mutex function and
+     * your thread implementation.
+     */
+
+    /**
+     * Set to true if the thread completed with success, false
+     * if an error occurred.
+     */
+    int *connfd;
+    int *packet_memory_fd;
+    struct sockaddr_in clnt_addr;
+    bool complete;
+    pthread_mutex_t *mutex;
+
+};
